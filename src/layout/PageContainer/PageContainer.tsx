adbe9b727d752154e1e6e4ae52da13147474bf11
Camilo <camilozv18@gmail.com>
2023-12-11 21:04:58 -0500

    Add Home Page and a Page container for future layout

---
+++ src/layout/PageContainer/PageContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import Home from '../../pages/Home/Home';
+
+const PageContainer = () => {
+  return (
+    <Box sx={{ m: '0.5rem' }}>
+      <Home />
+    </Box>
+  );
+};
+
+export default PageContainer;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DataTable } from '../../components';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Tickets</h1>
+      <DataTable columns={[]} rows={[]} />
+    </>
+  );
+};
+
+export default Home;
